7f64bd4fcf57d4f7d609a2ad2c37c8c88e348af8
Thanh Pham <phamthanh0705@gmail.com>
2020-10-19 23:06:32 -0400

    Make changes to add database

--- hello.py
+++ hello.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from datetime import datetime
+
 app = Flask(__name__)
 application = app
+
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ingredients.db'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
-class NameForm(FlaskForm):
+#Create database model
+class Ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    #Function to return string when we add sth
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
+
+class IngredientForm(FlaskForm):
     ingre = StringField('Ingredient to add', validators=[DataRequired()])
     submit = SubmitField('Add')
 
@@ -31,8 +52,22 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     ingre = None
-    form = NameForm()
+    form = IngredientForm()
     if form.validate_on_submit():
         ingre = form.ingre.data
+        new_ingre = Ingredients(name=ingre)
         form.ingre.data = ''
-    return render_template('index.html', form=form, ingre=ingre)
+
+        #Push to database
+        try:
+            db.session.add(new_ingre)
+            db.session.commit()
+            return redirect('/assignment09')
+        except:
+            return "There was an error adding new ingredient"
+    else:
+        ingredients = Ingredients.query.order_by(Ingredients.date_created)
+        return render_template('index.html', form=form, ingredients=ingredients)
+
+if __name__ == '__main__':
+    manager.run()
